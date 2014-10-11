798ff689c4e1ad44a117bb48a3ae27a3ea3edf66
Andrei Shevel <ash.developer@yandex.ru>
2014-10-12 01:46:07 +0300

    ng-typeahead directive

---
+++ ng-typeahead.js
@@ -0,0 +1,46 @@
+angular.module('ash', []).directive("ngTypeahead", function () {
+
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    scope: {
+      options: '=',
+      datasets: '='
+    },
+    link: function ($scope, element, attrs, ngModel) {
+
+      $scope.isChanged = false;
+
+      element.typeahead($scope.options || {}, $scope.datasets || {});
+
+      ngModel.$parsers.push(function (value) {
+        ngModel.$setValidity('typeahead', $scope.isChanged);
+
+        if ($scope.isChanged) {
+          $scope.isChanged = false;
+        } else {
+          value = undefined;
+        }
+
+        return value;
+      });
+
+      $scope.changeModel = function(value) {
+        $scope.$apply(function () {
+          $scope.isChanged = true;
+          ngModel.$setViewValue(value);
+        });
+      }
+
+      element.bind('typeahead:selected', function(event, suggestion) {
+        $scope.changeModel(suggestion);
+      });
+
+      element.bind('typeahead:autocompleted', function(event, suggestion, dataset) {
+        $scope.changeModel(suggestion);
+      });
+
+    }
+  }
+
+});
